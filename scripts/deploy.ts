8c19bb32101e551d58ec30e9e5a41131f68d6b32
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-10-20 02:22:26 +0100

    update the contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,44 @@
-import { ethers } from "hardhat";
+const main = async () => {
+  // Get 'OnChainNFT' contract
+  const nftContractFactory = await hre.ethers.getContractFactory('OnChainNFT');
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Deploy contract
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log('✅ Contract deployed to:', nftContract.address);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // SVG image that you want to mint
+  const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='1024' height='1024'>
+      <defs><clipPath id='a'><path d='M0 0h1024v1024H0z'/></clipPath></defs>
+      <g clip-path='url(#a)'>
+        <path d='M0 0h1024v1024H0z'/>
+        <path fill='#fff' d='M0 241h1024v20H0zM0 502h1024v20H0zM0 763h1024v20H0z'/>
+        <path fill='#fff' d='M241 0h20v1024h-20z'/>
+      </g>
+    </svg>`;
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Call the mint function from our contract
+  const txn = await nftContract.mint(svg);
+  const txnReceipt = await txn.wait();
 
-  await lock.deployed();
+  // Get the token id of the minted NFT (using our event)
+  const event = txnReceipt.events?.find((event) => event.event === 'Minted');
+  const tokenId = event?.args['tokenId'];
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
-}
+  console.log(
+    '🎨 Your minted NFT:',
+    `https://testnets.opensea.io/assets/${nftContract.address}/${tokenId}`
+  );
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
